422264dc52b99851ac8e39d3be1fc8e96b79282a
sclotaire <s.clotaire@hotmail.co.uk>
2024-02-19 17:10:34 +0200

    task 3 complete whoop!

--- db/app-model-controller/app.js
+++ db/app-model-controller/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const getTopics = require('./controller')
+const { getTopics, getApi } = require('./controller')
 
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
+
+app.get('/api', getApi);
+
 
 module.exports = app;
